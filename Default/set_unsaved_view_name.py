069a2633ab4681b979798d19704f2823c5766a26
Tanel Puhu <tanel@tanelpuhu.com>
2012-02-13 10:51:16 +0200

    Build 2177 Release Date: 10 February 2012

--- Default/set_unsaved_view_name.py
+++ Default/set_unsaved_view_name.py
@@ -15,7 +15,30 @@ class SetUnsavedViewName(sublime_plugin.EventListener):
             return
 
         cur_name = view.settings().get('auto_name')
+        view_name = view.name()
+
+        # Only set the name for plain text files
+        syntax = view.settings().get('syntax')
+        if syntax != 'Packages/Text/Plain text.tmLanguage':
+            if cur_name:
+                # Undo any previous name that was set
+                view.settings().erase('auto_name')
+                if cur_name == view_name:
+                    view.set_name("")
+            return
+
+        # Name has been explicitly set, don't override it
+        if not cur_name and view_name:
+            return
+
+        # Name has been explicitly changed, don't override it
         if cur_name and cur_name != view.name():
+            view.settings().erase('auto_name')
+            return
+
+        # Don't set the names on widgets, it'll just trigger spurious
+        # on_modified callbacks
+        if view.settings().get('is_widget'):
             return
 
         line = view.line(0)
@@ -23,10 +46,6 @@ class SetUnsavedViewName(sublime_plugin.EventListener):
             line = sublime.Region(0, 50)
 
         first_line = view.substr(line)
-
-        if (len(first_line) > 1 and first_line[0:2] == "#!"
-                or len(first_line) > 7 and first_line[0:8] == "#include"):
-            first_line = ""
 
         first_line = first_line.strip(self.dropped_chars)
 
